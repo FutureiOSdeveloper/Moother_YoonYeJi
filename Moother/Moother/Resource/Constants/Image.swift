94b790768f3867e411c45284f633575c353cf681
yejiyun <20172062@sungshin.ac.kr>
2021-07-30 21:54:39 +0900

    [#2] 셀, 이미지 상수명 추가

---
+++ Moother/Moother/Resource/Constants/Image.swift
@@ -0,0 +1,15 @@
+//
+//  Image.swift
+//  Moother
+//
+//  Created by 윤예지 on 2021/07/25.
+//
+
+import UIKit
+
+extension Const {
+    struct Image {
+        static let backgroundImage = UIImage(named: "background")
+        static let weatherIcon = UIImage(named: "11n")
+    }
+}
