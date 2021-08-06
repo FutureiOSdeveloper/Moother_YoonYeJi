7dc94dc43984475c45b49f915352539ae3cbd195
yejiyun <20172062@sungshin.ac.kr>
2021-08-06 19:55:57 +0900

    [#16] delegate protocol
    - 변수명 리네이밍
    - 푸터 버튼 이벤트 클릭 시 프로토콜 추가

---
+++ Moother/Moother/Source/Delegates/LoctaionDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  LocationIndexProtocol.swift
+//  Moother
+//
+//  Created by 윤예지 on 2021/08/06.
+//
+
+import UIKit
+
+protocol LoctaionDelegate {
+    func tableViewDidSelected(_ tableView: UITableView, at index: Int)
+}
